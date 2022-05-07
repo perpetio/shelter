1abfe6c3b9356a6e0416c63737184bfbfac49444
vpanyush <vpanyush@perpet.io>
2022-05-07 19:30:08 +0300

    implemented shelter marker UI

---
+++ lib/presentation/screens/map/local_widgets/shelter_marker.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:shelter/presentation/styles/styles.dart';
+
+import 'triangle_clipper.dart';
+
+class ShelterMarker extends Marker {
+  ShelterMarker(LatLng point)
+      : super(
+          point: point,
+          builder: _builder,
+          width: 24.w,
+          height: 28.5.w,
+          anchorPos: AnchorPos.align(AnchorAlign.top),
+        );
+
+  static Widget _builder(BuildContext context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            width: 24.w,
+            height: 24.w,
+            decoration: BoxDecoration(
+              color: ShColors.downy,
+              borderRadius: BorderRadius.circular(4.r),
+            ),
+            child: FaIcon(
+              FontAwesomeIcons.personShelter,
+              size: 15.w,
+              color: ShColors.black,
+            ),
+          ),
+          ClipPath(
+            clipper: TriangleClipper(),
+            child: Container(width: 8.w, height: 4.5.w, color: ShColors.downy),
+          )
+        ],
+      );
+}
--- lib/presentation/styles/sh_colors.dart
+++ lib/presentation/styles/sh_colors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ShColors {
+  static const Color black = Colors.black;
   static const Color boulder = Color(0xFF7A7A7A);
+  static const Color downy = Color(0xFF54D2B4);
   static const Color mineShaft = Color(0xFF232323);
   static const Color silver = Color(0xFFC4C4C4);
   static const Color sushi = Color(0xFF82C342);
