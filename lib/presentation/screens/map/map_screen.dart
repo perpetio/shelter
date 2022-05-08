cc789b2ebe3e713bff98c507ad7800756fb0120c
vpanyush <vpanyush@perpet.io>
2022-05-08 15:57:11 +0300

    implemented markers clustering

--- lib/presentation/screens/map/map_screen.dart
+++ lib/presentation/screens/map/map_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:shelter/data/models/models.dart';
 import 'package:shelter/domain/cubits/cubits.dart';
 import 'package:shelter/presentation/widgets/widgets.dart';
 
-import 'local_widgets/shelter_marker.dart';
-import 'map_tile_layer.dart';
+import 'local_widgets/map_tile_layer.dart';
+import 'local_widgets/shelters_marker_cluster_layer.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -46,14 +47,11 @@ class _MapScreenState extends State<MapScreen> {
             center: center,
             zoom: 13,
             maxZoom: 20,
+            plugins: [MarkerClusterPlugin()],
           ),
           layers: [
             MapTileLayer.layer(context),
-            MarkerLayerOptions(
-              markers: shelters
-                  .map((sh) => ShelterMarker(sh.properties.point!))
-                  .toList(),
-            ),
+            SheltersMarkerClusterLayer(shelters: shelters),
           ],
         ),
       );
